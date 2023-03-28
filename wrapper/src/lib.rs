3876e0b2f5a5e96c9c3f23626c8eaaac875c78e3
Axel Benjaminsson <axel.benjaminsson@live.se>
2023-03-28 08:16:47 +0200

    Add rust wrapper for adding account meta and signer

---
+++ wrapper/src/lib.rs
@@ -0,0 +1,85 @@
+#[macro_use]
+extern crate lazy_static;
+
+pub mod utils;
+
+use crate::utils::{
+    pointer_to_key,
+    pointers_to_keypair,
+};
+
+use std::sync::Mutex;
+use core::ffi::{
+    c_char,
+    c_int,
+};
+
+use solana_sdk::{
+    instruction::AccountMeta,
+    signer::keypair::Keypair,
+};
+
+pub struct AccountManager {
+    pub accounts: Vec<AccountMeta>,
+    pub signers: Vec<Keypair>,
+    //pub keypair: Keypair,
+    //pub program_id: Pubkey,
+    //pub rpc_url: String,
+    //pub signatures: Vec<Signature>,
+}
+
+lazy_static! {
+    static ref ACCOUNT_MANAGER: Mutex<AccountManager> = Mutex::new(AccountManager {
+        accounts: vec![],
+        signers: vec![],
+    });
+}
+
+#[no_mangle]
+pub extern fn append_account(account_pointer: *mut c_char) -> c_int {
+    // Convert the key
+    let pub_key = match pointer_to_key(account_pointer){
+        Ok(v) => v,
+        Err(e) => return e,
+    };
+
+    let mut account_manager = match ACCOUNT_MANAGER.lock(){
+        Ok(v) => v,
+        Err(_) => return -1,
+    };
+
+    // Append account to vector
+    account_manager.accounts.push(AccountMeta::new(pub_key, false));
+
+    0
+}
+
+#[no_mangle]
+pub extern fn append_signer(priv_key_pointer: *mut c_char, pub_key_pointer: *mut c_char) -> c_int{
+    // Convert the keys
+    let keypair = match pointers_to_keypair(priv_key_pointer, pub_key_pointer){
+        Ok(v) => v,
+        Err(e) => return e,
+    };
+
+    let mut account_manager = match ACCOUNT_MANAGER.lock(){
+        Ok(v) => v,
+        Err(_) => return -1,
+    };
+
+    // Append signer to vector
+    account_manager.signers.push(keypair);
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ wrapper/src/utils.rs
@@ -0,0 +1,52 @@
+use core::ffi::{
+    c_char,
+    c_int,
+    CStr,
+};
+
+use solana_sdk::{
+    pubkey::Pubkey,
+    signer::keypair::Keypair,
+};
+
+pub fn pointer_to_key(pointer: *mut c_char) -> Result<Pubkey, c_int>{
+    let key_string = unsafe { CStr::from_ptr(pointer).to_string_lossy().into_owned() };
+
+    let decoded_key = match bs58::decode(&key_string).into_vec() {
+        Ok(v) => v,
+        Err(_) => return Err(-1),
+    };
+
+    let key = match Pubkey::try_from(decoded_key.as_slice()){
+        Ok(v) => v,
+        Err(_) => return Err(-1)
+    };
+
+    Ok(key)
+}
+
+pub fn pointers_to_keypair(priv_key_pointer: *mut c_char, pub_key_pointer: *mut c_char) -> Result<Keypair, c_int>{
+    let priv_key_string = unsafe { CStr::from_ptr(priv_key_pointer).to_string_lossy().into_owned() };
+    let pub_key_string = unsafe { CStr::from_ptr(pub_key_pointer).to_string_lossy().into_owned() };
+
+    // Set private bytes
+    let mut keypair_bytes = match bs58::decode(&priv_key_string).into_vec() {
+        Ok(v) => v,
+        Err(_) => return Err(-1),
+    };
+
+    let mut pubkey_bytes = match bs58::decode(&pub_key_string).into_vec() {
+        Ok(v) => v,
+        Err(_) => return Err(-1),
+    };
+
+    // Append public bytes
+    keypair_bytes.append(&mut pubkey_bytes);
+
+    let keypair = match Keypair::from_bytes(&keypair_bytes){
+        Ok(v) => v,
+        Err(_) => return Err(-1),
+    };
+
+    Ok(keypair)
+}
