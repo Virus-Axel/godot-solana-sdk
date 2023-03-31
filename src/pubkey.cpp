c936204a3ee53a4e8a4594fd2d72f747dad77746
Axel Benjaminsson <axel.benjaminsson@live.se>
2023-03-31 23:10:33 +0200

    Add properties to pubkey class

--- src/pubkey.cpp
+++ src/pubkey.cpp
@@ -1,10 +1,70 @@
 #include "pubkey.hpp"
+#include "utils.hpp"
 
 #include <godot_cpp/core/class_db.hpp>
 
 namespace godot{
 
 void Pubkey::_bind_methods() {
+}
+
+bool Pubkey::_set(const StringName &p_name, const Variant &p_value) {
+	String name = p_name;
+	if (name == "value") {
+		value = p_value;
+        PackedByteArray decoded_value = bs58_decode(value);
+        bytes = decoded_value;
+        if (decoded_value.size() != 32){
+            internal::gde_interface->print_warning("Pubkey must be 32 bytes", "_set", "pubkey.cpp", 14, false);
+        }
+		return true;
+	}
+    else if(name == "bytes"){
+        bytes = p_value;
+        if (bytes.size() == 0){
+            value = "";
+            return true;
+        }
+        String encoded_value = bs58_encode(bytes);
+        value = encoded_value;
+        if (bytes.size() != 32){
+            internal::gde_interface->print_warning("Pubkey must be 32 bytes", "_set", "pubkey.cpp", 14, false);
+        }
+		return true;
+    }
+    else if(name == "unique"){
+        unique = p_value;
+        notify_property_list_changed();
+        return true;
+    }
+	return false;
+}
+
+bool Pubkey::_get(const StringName &p_name, Variant &r_ret) const {
+	String name = p_name;
+	if (name == "value") {
+		r_ret = value;
+		return true;
+	}
+    else if (name == "bytes") {
+		r_ret = bytes;
+		return true;
+	}
+    else if (name == "unique") {
+		r_ret = unique;
+		return true;
+	}
+	return false;
+}
+
+void Pubkey::_get_property_list(List<PropertyInfo> *p_list) const {
+    PropertyUsageFlags visibility = PROPERTY_USAGE_DEFAULT;
+    if(unique){
+        visibility = PROPERTY_USAGE_NO_EDITOR;
+    }
+    p_list->push_back(PropertyInfo(Variant::BOOL, "unique"));
+	p_list->push_back(PropertyInfo(Variant::STRING, "value", PROPERTY_HINT_NONE, "", visibility, ""));
+    p_list->push_back(PropertyInfo(Variant::PACKED_BYTE_ARRAY, "bytes", PROPERTY_HINT_NONE, "", visibility, ""));
 }
 
 Pubkey::Pubkey() {
--- src/pubkey.hpp
+++ src/pubkey.hpp
@@ -9,10 +9,16 @@ class Pubkey : public Node {
     GDCLASS(Pubkey, Node)
 
 private:
+    bool unique = true;
+    String value = "";
+    PackedByteArray bytes;
     void *data_pointer;
 
 protected:
     static void _bind_methods();
+    bool _set(const StringName &p_name, const Variant &p_value);
+	bool _get(const StringName &p_name, Variant &r_ret) const;
+    void _get_property_list(List<PropertyInfo> *p_list) const;
 
 public:
     Pubkey();
