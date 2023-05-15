94168e4049b90f668c424c68efd794deae0343d2
Axel Benjaminsson <axel.benjaminsson@live.se>
2023-05-15 21:35:31 +0200

    Implement associated token pubkeys

--- wrapper/wrapper.h
+++ wrapper/wrapper.h
@@ -11,6 +11,7 @@ extern "C"{
     void *create_unique_pubkey();
     void *create_pubkey_from_array(const unsigned char* data);
     void *create_pubkey_with_seed(const void* basePubkey, const char* seed, unsigned int seed_length, const void* owner_pubkey);
+    void *create_associated_token_account(const void* basePubkey, const void* owner_pubkey);
 
     void print_pubkey(const void*);
     void free_pubkey(const void*);
