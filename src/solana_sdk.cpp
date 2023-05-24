f9b2ae3c79a77a41874fc8ac94dfc05327ea7407
Axel Benjaminsson <axel.benjaminsson@live.se>
2023-05-24 19:31:06 +0200

    Add setting for rpc url

--- src/solana_sdk.cpp
+++ src/solana_sdk.cpp
@@ -17,10 +17,70 @@ void SolanaSDK::_bind_methods() {
     ClassDB::bind_static_method("SolanaSDK", D_METHOD("bs58_encode", "input"), &SolanaSDK::bs58_encode);
     ClassDB::bind_static_method("SolanaSDK", D_METHOD("bs58_decode", "input"), &SolanaSDK::bs58_decode);
     ClassDB::bind_static_method("SolanaSDK", D_METHOD("get_latest_blockhash"), &SolanaSDK::get_latest_blockhash);
+    ClassDB::bind_static_method("SolanaSDK", D_METHOD("set_url", "url"), &SolanaSDK::set_url);
+	ClassDB::bind_static_method("SolanaSDK", D_METHOD("get_url"), &SolanaSDK::get_url);
 }
 
 SolanaSDK::SolanaSDK() {
-    // initialize any variables here
+	url = TESTNET_URL;
+}
+
+Dictionary SolanaSDK::quick_http_request(const String& request_body){
+	const int32_t POLL_DELAY_MSEC = 10;
+
+	// Set headers
+	PackedStringArray http_headers;
+	http_headers.append("Content-Type: application/json");
+	http_headers.append("Accept-Encoding: json");
+	
+	// Connect to RPC API URL.
+	HTTPClient handler;
+	Error err = handler.connect_to_host(url, 443, TLSOptions::client_unsafe());
+
+	// Wait until a connection is established.
+	godot::HTTPClient::Status status = handler.get_status();
+	while(status == HTTPClient::STATUS_CONNECTING || status == HTTPClient::STATUS_RESOLVING){
+		handler.poll();
+		OS::get_singleton()->delay_msec(POLL_DELAY_MSEC);
+		status = handler.get_status();
+	}
+
+	// Make a POST request
+	err = handler.request(godot::HTTPClient::METHOD_POST, "/", http_headers, request_body);
+
+	if(err != Error::OK){
+		gde_interface->print_error("Error sending request.", "quick_http_request", "solana_sdk.cpp", __LINE__, false);
+		return Dictionary();
+	}
+
+	// Poll until we have a response.
+	status = handler.get_status();
+	while(status == HTTPClient::STATUS_REQUESTING){
+		handler.poll();
+		OS::get_singleton()->delay_msec(POLL_DELAY_MSEC);
+		status = handler.get_status();
+	}
+
+	// Collect the response body.
+	PackedByteArray response_data;
+	while(status == HTTPClient::STATUS_BODY){
+		response_data.append_array(handler.read_response_body_chunk());
+		handler.poll();
+		status = handler.get_status();
+	}
+
+	handler.close();
+
+	// Parse out the blockhash.
+	JSON json;
+	err = json.parse(response_data.get_string_from_utf8());
+
+	if(err != Error::OK){
+		gde_interface->print_error("Error getting response data.", "quick_http_request", "solana_sdk.cpp", __LINE__, false);
+		return Dictionary();
+	}
+
+	return json.get_data();
 }
 
 PackedByteArray SolanaSDK::bs58_decode(String str){
@@ -46,63 +106,10 @@ String SolanaSDK::bs58_encode(PackedByteArray input){
 }
 
 String SolanaSDK::get_latest_blockhash(){
-	const int32_t POLL_DELAY_MSEC = 10;
-	const godot::String RPC_URL = "https://api.devnet.solana.com";
 	const godot::String REQUEST_DATA = "{\"id\":1,\"jsonrpc\":\"2.0\",\"method\":\"getLatestBlockhash\",\"params\":[{\"commitment\":\"finalized\"}]}";
 
-	// Set headers
-	PackedStringArray http_headers;
-	http_headers.append("Content-Type: application/json");
-	http_headers.append("Accept-Encoding: json");
-	
-	// Connect to RPC API URL.
-	HTTPClient handler;
-	Error err = handler.connect_to_host(RPC_URL, 443, TLSOptions::client_unsafe());
+	Dictionary data = quick_http_request(REQUEST_DATA);
 
-	// Wait until a connection is established.
-	godot::HTTPClient::Status status = handler.get_status();
-	while(status == HTTPClient::STATUS_CONNECTING || status == HTTPClient::STATUS_RESOLVING){
-		handler.poll();
-		OS::get_singleton()->delay_msec(POLL_DELAY_MSEC);
-		status = handler.get_status();
-	}
-
-	// Make a POST request
-	err = handler.request(godot::HTTPClient::METHOD_POST, "/", http_headers, REQUEST_DATA);
-
-	if(err != Error::OK){
-		gde_interface->print_error("Error sending request.", "get_latest_blockhash", "solana_sdk.cpp", __LINE__, false);
-		return "";
-	}
-
-	// Poll until we have a response.
-	status = handler.get_status();
-	while(status == HTTPClient::STATUS_REQUESTING){
-		handler.poll();
-		OS::get_singleton()->delay_msec(POLL_DELAY_MSEC);
-		status = handler.get_status();
-	}
-
-	// Collect the response body.
-	PackedByteArray response_data;
-	while(status == HTTPClient::STATUS_BODY){
-		response_data.append_array(handler.read_response_body_chunk());
-		handler.poll();
-		status = handler.get_status();
-	}
-
-	handler.close();
-
-	// Parse out the blockhash.
-	JSON json;
-	err = json.parse(response_data.get_string_from_utf8());
-
-	if(err != Error::OK){
-		gde_interface->print_error("Error getting response data.", "get_latest_blockhash", "solana_sdk.cpp", __LINE__, false);
-		return "";
-	}
-
-	Dictionary data = json.get_data();
 	if(!data.has("result")){
 		gde_interface->print_error("Unexpected response.", "get_latest_blockhash", "solana_sdk.cpp", __LINE__, false);
 		return "";
@@ -122,65 +129,17 @@ String SolanaSDK::get_latest_blockhash(){
 }
 
 Variant SolanaSDK::send_transaction(const String& transaction){
-	const int32_t POLL_DELAY_MSEC = 10;
-	const godot::String RPC_URL = "https://api.devnet.solana.com";
 	const godot::String REQUEST_DATA = "{\"id\":1,\"jsonrpc\":\"2.0\",\"method\":\"sendTransaction\",\"params\":[\"" + transaction + "\",{\"encoding\":\"base64\"}]}";
 
-	std::cout << REQUEST_DATA.to_utf8_buffer().ptr() << std::endl;
+	return quick_http_request(REQUEST_DATA);
+}
 
-	// Set headers
-	PackedStringArray http_headers;
-	http_headers.append("Content-Type: application/json");
-	http_headers.append("Accept-Encoding: json");
-	
-	// Connect to RPC API URL.
-	HTTPClient handler;
-	Error err = handler.connect_to_host(RPC_URL, 443, TLSOptions::client_unsafe());
+void SolanaSDK::set_url(const String& url){
+	SolanaSDK::url = url;
+}
 
-	// Wait until a connection is established.
-	godot::HTTPClient::Status status = handler.get_status();
-	while(status == HTTPClient::STATUS_CONNECTING || status == HTTPClient::STATUS_RESOLVING){
-		handler.poll();
-		OS::get_singleton()->delay_msec(POLL_DELAY_MSEC);
-		status = handler.get_status();
-	}
-
-	// Make a POST request
-	err = handler.request(godot::HTTPClient::METHOD_POST, "/", http_headers, REQUEST_DATA);
-
-	if(err != Error::OK){
-		gde_interface->print_error("Error sending request.", "send_transaction", "solana_sdk.cpp", __LINE__, false);
-		return "";
-	}
-
-	// Poll until we have a response.
-	status = handler.get_status();
-	while(status == HTTPClient::STATUS_REQUESTING){
-		handler.poll();
-		OS::get_singleton()->delay_msec(POLL_DELAY_MSEC);
-		status = handler.get_status();
-	}
-
-	// Collect the response body.
-	PackedByteArray response_data;
-	while(status == HTTPClient::STATUS_BODY){
-		response_data.append_array(handler.read_response_body_chunk());
-		handler.poll();
-		status = handler.get_status();
-	}
-
-	handler.close();
-
-	// Parse out the blockhash.
-	JSON json;
-	err = json.parse(response_data.get_string_from_utf8());
-
-	if(err != Error::OK){
-		gde_interface->print_error("Error getting response data.", "send_transaction", "solana_sdk.cpp", __LINE__, false);
-		return "";
-	}
-
-	return json.get_data();
+String SolanaSDK::get_url(){
+	return SolanaSDK::url;
 }
 
 SolanaSDK::~SolanaSDK() {
