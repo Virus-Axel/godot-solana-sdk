b7ae8557790e4976a7bf0e39b4c555650c4df606
Axel Benjaminsson <axel.benjaminsson@live.se>
2023-06-05 23:45:50 +0200

    Add a property of type pubkey

--- wrapper/src/modules/pubkey.rs
+++ wrapper/src/modules/pubkey.rs
@@ -1,21 +1,44 @@
 use godot::engine::Node;
+use godot::engine::global::PropertyHint;
+use godot::obj::dom::UserDomain;
+use godot::obj::mem::{StaticRefCount, DynamicRefCount};
 use godot::prelude::*;
 use godot::prelude::GodotClass;
 use godot::engine::NodeVirtual;
 use godot::builtin::GodotString;
+use godot::export::Export;
 
 use solana_sdk::pubkey;
 
+struct pubkey_internal{
+    key: pubkey::Pubkey,
+}
+
+impl Export for pubkey_internal{
+    fn default_export_info() -> godot::export::ExportInfo {
+        godot::export::ExportInfo {hint: PropertyHint::PROPERTY_HINT_NONE, hint_string: GodotString::new(), variant_type: VariantType::Object}
+    }
+    fn export(&self) -> Self {
+        pubkey_internal { key: pubkey::Pubkey::new_unique() }
+    }
+}
+
+impl GodotClass for pubkey_internal{
+    type Base = Node;
+    type Declarer = UserDomain;
+    type Mem = DynamicRefCount;
+    const CLASS_NAME: &'static str = "public key";
+}
 
 #[derive(GodotClass)]
 #[class(base=Node)]
 struct Pubkey {
     #[export]
-    key: GodotString,
+    key: Gd<pubkey_internal>,
     #[export]
     seed: GodotString,
     #[export]
-    base_key: Gd<Pubkey>,
+    base_key: Gd<pubkey_internal>,
 
     #[base]
     base: Base<Node>
@@ -23,9 +46,6 @@ struct Pubkey {
 
 #[godot_api]
 impl Pubkey {
-    fn empty() -> Pubkey{
-        Pubkey {base: Base::<Node>::from(Node::new_alloc().into()), key: GodotString::new(), seed: GodotString::new(), base_key: Gd::new_default() }
-    }
 
     #[func]
     fn increase_speed(&mut self, body: Gd<Pubkey>) {
@@ -33,7 +53,7 @@ impl Pubkey {
     }
     #[func]
     fn print(&mut self) -> GodotString{
-        return GodotString::from(self.key_object.to_string());
+        return GodotString::from(self.key.to_string());
     }
 
     #[signal]
@@ -43,6 +63,11 @@ impl Pubkey {
 #[godot_api]
 impl NodeVirtual for Pubkey {
     fn init(base: Base<Self::Base>) -> Self {
-        Self { base, key: GodotString::new(), base_key: Pubkey::empty() }
+        Self {
+            base,
+            key: Gd::new(pubkey_internal { key: pubkey::Pubkey::new_unique() }),
+            base_key: Gd::new(pubkey_internal { key: pubkey::Pubkey::new_unique() }),
+            seed: GodotString::new(),
+        }
     }
 }
