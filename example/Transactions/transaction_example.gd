d113bea04a06047abacf5e26057031cca3fbfa2e
Axel Benjaminsson <axel.benjaminsson@live.se>
2024-05-03 09:59:18 +0200

    Add transaction confirmation signals
    The way to check for confirmation requires users to poll confirmation
    statuses in a loop. This commit adds signals so the user can connect them
    instead. The signals utilize the ws client to subscribe to the signature
    if there is a connection made to the signal.

--- example/Transactions/transaction_example.gd
+++ example/Transactions/transaction_example.gd
@@ -4,7 +4,7 @@ extends VBoxContainer
 @onready var payer: Keypair = Keypair.new_from_file("res://payer.json")
 const LAMPORTS_PER_SOL = 1000000000
 
-const TOTAL_CASES := 3
+const TOTAL_CASES := 10
 var passed_test_mask := 0
 
 
@@ -18,7 +18,7 @@ func transaction_example_airdrop_to():
 	# so building a transaction is not necessary
 	
 
-	$SolanaClient.request_airdrop(payer.get_public_string(), LAMPORTS_PER_SOL)
+	$SolanaClient.request_airdrop(payer.get_public_string(), 2 * LAMPORTS_PER_SOL)
 	var response = await $SolanaClient.http_response_received
 
 	# Error check the RPC result
@@ -61,6 +61,7 @@ func transaction_example_transfer():
 	assert(typeof(signature) == TYPE_STRING)
 	
 	$TransferSolanaLabel.text += signature
+	
 	PASS(1)
 
 
@@ -94,6 +95,76 @@ func create_account_example():
 	PASS(2)
 
 
+func transaction_with_confirmation_1():
+	# Transfer instruction with connections before getting signature.
+	
+	var receiver: Pubkey = Pubkey.new_random()
+	var tx = Transaction.new()
+
+	add_child(tx)
+	
+	tx.set_payer(payer)
+	
+	var ix: Instruction = SystemProgram.transfer(payer, receiver, LAMPORTS_PER_SOL / 10)
+	tx.add_instruction(ix)
+	
+	tx.update_latest_blockhash()
+
+	tx.connect("processed", Callable(self, "transaction_processed"))
+	tx.connect("confirmed", Callable(self, "transaction_confirmed"))
+	tx.connect("finalized", Callable(self, "transaction_finalized"))
+	tx.sign_and_send()
+	
+	var response = await tx.transaction_response_received
+	
+	assert(response.has("result"))
+	var signature = response["result"]
+	assert(typeof(signature) == TYPE_STRING)
+	
+	PASS(3)
+
+
+func transaction_processed():
+	PASS(4)
+
+func transaction_confirmed():
+	PASS(5)
+	
+func transaction_finalized():
+	PASS(6)
+
+
+func transaction_with_confirmation_2():
+	# Transfer instruction with connections before after signature.
+	
+	var receiver: Pubkey = Pubkey.new_random()
+	var tx = Transaction.new()
+
+	add_child(tx)
+	
+	tx.set_payer(payer)
+	
+	var ix: Instruction = SystemProgram.transfer(payer, receiver, LAMPORTS_PER_SOL / 10)
+	tx.add_instruction(ix)
+	
+	tx.update_latest_blockhash()
+
+	tx.sign_and_send()
+	
+	var response = await tx.transaction_response_received
+	
+	assert(response.has("result"))
+	var signature = response["result"]
+	assert(typeof(signature) == TYPE_STRING)
+	
+	await tx.processed
+	PASS(7)
+	await tx.confirmed
+	PASS(8)
+	await tx.finalized
+	PASS(9)
+
+
 func _ready():
 	# Use a local cluster for unlimited Solana airdrops.
 	# SolanaClient defaults to devnet cluster URL if not specified.
@@ -105,6 +176,8 @@ func _ready():
 	
 	transaction_example_transfer()
 	create_account_example()
+	transaction_with_confirmation_1()
+	transaction_with_confirmation_2()
 
 
 func _on_timeout_timeout():
