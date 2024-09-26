2eff5d001fbd8127e59cc5a95c22bc1049b594d7
Axel Benjaminsson <axel.benjaminsson@live.se>
2024-09-26 21:05:07 +0200

    Almost done with implementation

--- include/shdwdrive.hpp
+++ include/shdwdrive.hpp
@@ -7,6 +7,26 @@
 
 namespace godot{
 
+class StorageAccountV2: public Resource{
+GDCLASS(StorageAccountV2, Resource)
+private:
+    bool immutable = false;
+    bool to_be_deleted = false;
+    uint32_t delete_request_epoch = 0;
+    uint64_t storage = 0;
+    Variant owner1;
+    uint32_t account_counter_seed = 0;
+    uint32_t creation_time = 0;
+    uint32_t creation_epoch = 0;
+    uint32_t last_fee_epoch = 0;
+    String identifier = "";
+
+    static PackedByteArray discriminator();
+
+public:
+    void from_bytes(const PackedByteArray& bytes);
+};
+
 class UserInfo: public Resource{
 GDCLASS(UserInfo, Resource)
 private:
@@ -24,15 +44,28 @@ public:
 class ShdwDrive : public Node {
 GDCLASS(ShdwDrive, Node)
 private:
+    static const std::string ShdwDrive::ID;
+
+    UserInfo* user_info = nullptr;
+    StorageAccountV2* storage_account = nullptr;
+
     SolanaClient *fetch_user_account_client;
 
     void fetch_userinfo_callback(const Dictionary& params);
+    void fetch_storage_account_callback(const Dictionary& params);
     uint64_t human_size_to_bytes(const String& human_size);
 protected:
     static void _bind_methods();
 public:
-    Variant fetch(const Variant address);
-    Variant create_storage_account(const String& name, const String& size);
+    Variant fetch_user_info(const Variant address);
+    Variant fetch_storage_account(const Variant address);
+    Variant create_storage_account(const Variant& owner_keypair, const String& name, const String& size);
+
+    void send_create_storage_tx();
+
+    static Variant new_user_info_pubkey(const Variant& base_keypair);
+
+    static Variant get_pid();
 };
 
 } // godot
--- src/shdwdrive.cpp
+++ src/shdwdrive.cpp
@@ -2,8 +2,46 @@
 
 #include "solana_client.hpp"
 #include <godot_cpp/classes/engine.hpp>
+#include <pubkey.hpp>
+#include <keypair.hpp>
 
 namespace godot{
+
+const std::string ShdwDrive::ID = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s";
+
+PackedByteArray StorageAccountV2::discriminator(){
+    const unsigned char DISCRIMINATOR_ARRAY[] = {133, 53, 253, 82, 212, 5, 201, 218};
+    PackedByteArray result;
+    for (unsigned int i = 0; i < 8; i++){
+        result.append(DISCRIMINATOR_ARRAY[i]);
+    }
+    return result;
+}
+
+void StorageAccountV2::from_bytes(const PackedByteArray& bytes){
+    ERR_FAIL_COND_EDMSG(bytes.slice(0, 8) == discriminator(), "Account is not StorageAccountV2.");
+
+    int c = 8;
+    immutable = bytes[c++];
+    to_be_deleted = bytes[c++];
+    delete_request_epoch = bytes.decode_u32(c);
+    c += 4;
+    storage = bytes.decode_u64(c);
+    c += 8;
+    owner1 = Pubkey::new_from_bytes(bytes.slice(c, c + 32));
+    c += 32;
+    account_counter_seed = bytes.decode_u32(c);
+    c += 4;
+    creation_time = bytes.decode_u32(c);
+    c += 4;
+    creation_epoch = bytes.decode_u32(c);
+    c += 4;
+    last_fee_epoch = bytes.decode_u32(c);
+    c += 4;
+    uint32_t identifier_size = bytes.decode_u32(c);
+    c += 4;
+    identifier = bytes.slice(c, c + identifier_size).get_string_from_ascii();
+}
 
 PackedByteArray UserInfo::discriminator(){
     const unsigned char DISCRIMINATOR_ARRAY[] = {83, 134, 200, 56, 144, 56, 10, 62};
@@ -23,11 +61,22 @@ void UserInfo::from_bytes(const PackedByteArray& bytes){
     had_bad_scam_scan = bytes[17] == 1;
 }
 
-Variant ShdwDrive::fetch(const Variant address){
+Variant ShdwDrive::fetch_user_info(const Variant address){
     UserInfo *user_info = memnew(UserInfo);
     
     SolanaClient *client = memnew(SolanaClient);
     Callable callback = Callable(this, "fetch_userinfo_callback");
+
+    client->set_callback(callback);
+    client->get_account_info(address);
+    return OK;
+}
+
+Variant ShdwDrive::fetch_storage_account(const Variant address){
+    StorageAccountV2 *storage_account = memnew(StorageAccountV2);
+    
+    SolanaClient *client = memnew(SolanaClient);
+    Callable callback = Callable(this, "fetch_storage_account_callback");
 
     client->set_callback(callback);
     client->get_account_info(address);
@@ -50,17 +99,66 @@ void ShdwDrive::fetch_userinfo_callback(const Dictionary& params){
     Dictionary account = ((Dictionary)params["result"])["value"];
     const PackedByteArray data_info = account["data"];
 
+    UserInfo* user_info = memnew(UserInfo);
+    user_info->from_bytes(data_info);
 
+    emit_signal("userinfo_fetched", user_info);
+}
+
+void ShdwDrive::fetch_storage_account_callback(const Dictionary& params){
+    // TODO(Virax): Add helper for this common pattern.
+    if(!params.has("result")){
+        return;
+    }
+    if(!((Dictionary)params["result"]).has("value")){
+        return;
+    }
+
+    Dictionary account = ((Dictionary)params["result"])["value"];
+    const PackedByteArray data_info = account["data"];
+
+    StorageAccountV2* storage_account = memnew(StorageAccountV2);
+    storage_account->from_bytes(data_info);
+
+    emit_signal("storage_account_fetched", storage_account);
 }
 
 void ShdwDrive::_bind_methods(){
-    ClassDB::add_signal("ShdwDrive", MethodInfo("userinfo_fetched"));
+    ClassDB::add_signal("ShdwDrive", MethodInfo("user_info_fetched", PropertyInfo(Variant::OBJECT, "user_info")));
+    ClassDB::add_signal("ShdwDrive", MethodInfo("storage_account_fetched", PropertyInfo(Variant::OBJECT, "storage_account")));
 
-    ClassDB::bind_method(D_METHOD("fetch_userinfo_callback"), &ShdwDrive::fetch_userinfo_callback);
+    ClassDB::bind_method(D_METHOD("fetch_userinfo_callback", "params"), &ShdwDrive::fetch_userinfo_callback);
+    ClassDB::bind_method(D_METHOD("fetch_storage_account_callback", "params"), &ShdwDrive::fetch_storage_account_callback);
 }
 
-Variant ShdwDrive::create_storage_account(const String& name, const String& size){
- return nullptr;
+Variant ShdwDrive::create_storage_account(const Variant& owner_keypair, const String& name, const String& size){
+    Callable callback = Callable(this, "send_create_storage_tx");
+
+    connect("user_info_fetched", callback, CONNECT_ONE_SHOT);
+    connect("storage_account_fetched", callback, CONNECT_ONE_SHOT);
+
+    fetch_user_info(new_user_info_pubkey(owner_keypair));
+    fetch_storage_account(owner_keypair);
+
+    return nullptr;
+}
+
+void ShdwDrive::send_create_storage_tx(){
+    ERR_FAIL_COND(user_info == nullptr);
+    ERR_FAIL_COND(storage_account == nullptr);
+
+
+}
+
+Variant ShdwDrive::new_user_info_pubkey(const Variant& base_keypair){
+    Array seeds;
+    seeds.append(String("user-info").to_ascii_buffer());
+    seeds.append(Object::cast_to<Keypair>(base_keypair));
+    return Pubkey::new_pda_bytes(seeds, get_pid());
+}
+
+Variant ShdwDrive::get_pid(){
+    return Pubkey::new_from_string(ID.c_str());
 }
 
 }
