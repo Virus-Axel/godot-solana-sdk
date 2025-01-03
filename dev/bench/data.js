3c1d2d32295f906f6894f125ab350187861ded6e
github-action-benchmark <github@users.noreply.github.com>
2025-01-03 21:43:53 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for a8b07a74183b405a326185d26b7b726f3d44b7d8

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1735940633120,
+  "repoUrl": "https://github.com/Virus-Axel/godot-solana-sdk",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "axel.benjaminsson@live.se",
+            "name": "Virus-Axel",
+            "username": "Virus-Axel"
+          },
+          "committer": {
+            "email": "axel.benjaminsson@live.se",
+            "name": "Virus-Axel",
+            "username": "Virus-Axel"
+          },
+          "distinct": true,
+          "id": "a8b07a74183b405a326185d26b7b726f3d44b7d8",
+          "message": "Add simple benchmark action",
+          "timestamp": "2025-01-03T22:37:05+01:00",
+          "tree_id": "77a4c85fc43599d294e4efd24df0463328e78319",
+          "url": "https://github.com/Virus-Axel/godot-solana-sdk/commit/a8b07a74183b405a326185d26b7b726f3d44b7d8"
+        },
+        "date": 1735940632797,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Simple Transaction Performance Index",
+            "value": 0.993150684931507,
+            "unit": "Percent"
+          }
+        ]
+      }
+    ]
+  }
+}
