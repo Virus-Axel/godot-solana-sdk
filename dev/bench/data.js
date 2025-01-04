1d38cbae8f5d1732d6e52385c4244fad7c1814cd
github-action-benchmark <github@users.noreply.github.com>
2025-01-04 09:31:31 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for fe52a3709305a765093c6f23735227e61028011f

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1735983091530,
+  "repoUrl": "https://github.com/Virus-Axel/godot-solana-sdk",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "axel.benjaminsson@live.se",
+            "name": "Axel",
+            "username": "Virus-Axel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fe52a3709305a765093c6f23735227e61028011f",
+          "message": "Merge pull request #312 from Virus-Axel/feature/simple_benchmark\n\nFix doxygen deployment removing benchmark data",
+          "timestamp": "2025-01-04T10:09:05+01:00",
+          "tree_id": "93c93c9672b451565989173469735057dd44c97f",
+          "url": "https://github.com/Virus-Axel/godot-solana-sdk/commit/fe52a3709305a765093c6f23735227e61028011f"
+        },
+        "date": 1735983090962,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Simple Transaction Performance Index",
+            "value": 0.164383561643836,
+            "unit": "Percent"
+          }
+        ]
+      }
+    ]
+  }
+}
