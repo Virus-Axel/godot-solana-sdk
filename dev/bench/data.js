4c4d837b6299536e1fa184b7c3d948b6a236fdf0
github-action-benchmark <github@users.noreply.github.com>
2025-01-04 09:45:54 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for b9eff525c1e19b641c820e829736cc3ef802b0ce

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735983091530,
+  "lastUpdate": 1735983954482,
   "repoUrl": "https://github.com/Virus-Axel/godot-solana-sdk",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/Virus-Axel/godot-solana-sdk/commit/fe52a3709305a765093c6f23735227e61028011f"
         },
         "date": 1735983090962,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Simple Transaction Performance Index",
+            "value": 0.164383561643836,
+            "unit": "Percent"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "axel.benjaminsson@live.se",
+            "name": "Axel",
+            "username": "Virus-Axel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b9eff525c1e19b641c820e829736cc3ef802b0ce",
+          "message": "Fix typo",
+          "timestamp": "2025-01-04T10:27:26+01:00",
+          "tree_id": "3b9056db0122f02e1b8dfa19b2b3701617193cc5",
+          "url": "https://github.com/Virus-Axel/godot-solana-sdk/commit/b9eff525c1e19b641c820e829736cc3ef802b0ce"
+        },
+        "date": 1735983953294,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
