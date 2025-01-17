d0eae6b927280775c7389c7781c68550e709de90
Virus-Axel <axel.benjaminsson@live.se>
2025-01-17 17:41:22 +0000

    Deploying to doxygen-docs from @ Virus-Axel/godot-solana-sdk@8fe1225e11fd28f17aabf1f28182c519c3ac7d23 🚀

--- docs/search/all_b.js
+++ docs/search/all_b.js
@@ -5,8 +5,9 @@ var searchData=
   ['new_5ffrom_5ffile_2',['new_from_file',['../classgodot_1_1_keypair.html#a7107c395e92a58810b6086e4168c5089',1,'godot::Keypair']]],
   ['new_5ffrom_5fseed_3',['new_from_seed',['../classgodot_1_1_keypair.html#a895cbd3904899aa05eac2d21e6edd600',1,'godot::Keypair::new_from_seed(const String &amp;seed)'],['../classgodot_1_1_keypair.html#ab5c1204c95342e90af0152b44206b0cc',1,'godot::Keypair::new_from_seed(const PackedByteArray &amp;seed)'],['../classgodot_1_1_pubkey.html#a24f9083a51d848b68723291aa7dc092b',1,'godot::Pubkey::new_from_seed(Variant basePubkey, String seed, Variant owner_pubkey)']]],
   ['new_5ffrom_5fstring_4',['new_from_string',['../classgodot_1_1_pubkey.html#ab62c2ec05865ac01454c438fdbd4adac',1,'godot::Pubkey']]],
-  ['new_5fpda_5',['new_pda',['../classgodot_1_1_pubkey.html#ac4ff39b4fc9eeb2f3a35d9658d33f849',1,'godot::Pubkey']]],
-  ['new_5fpda_5fbytes_6',['new_pda_bytes',['../classgodot_1_1_pubkey.html#a4fa5df3a5cda1d70ec0cde10a96e4816',1,'godot::Pubkey']]],
-  ['new_5fprogram_5faddress_7',['new_program_address',['../classgodot_1_1_pubkey.html#aacbd88cc1523cc97e7a659578c9af7bf',1,'godot::Pubkey']]],
-  ['new_5frandom_8',['new_random',['../classgodot_1_1_keypair.html#a311ab62b9b7187be519879d8c4cc1924',1,'godot::Keypair::new_random()'],['../classgodot_1_1_pubkey.html#aa9ccf03afa613628bcca250399c00ded',1,'godot::Pubkey::new_random()']]]
+  ['new_5ffrom_5fvariant_5',['new_from_variant',['../classgodot_1_1_pubkey.html#a58669382f314ea379021a9c464c2038b',1,'godot::Pubkey']]],
+  ['new_5fpda_6',['new_pda',['../classgodot_1_1_pubkey.html#ac4ff39b4fc9eeb2f3a35d9658d33f849',1,'godot::Pubkey']]],
+  ['new_5fpda_5fbytes_7',['new_pda_bytes',['../classgodot_1_1_pubkey.html#a4fa5df3a5cda1d70ec0cde10a96e4816',1,'godot::Pubkey']]],
+  ['new_5fprogram_5faddress_8',['new_program_address',['../classgodot_1_1_pubkey.html#aacbd88cc1523cc97e7a659578c9af7bf',1,'godot::Pubkey']]],
+  ['new_5frandom_9',['new_random',['../classgodot_1_1_keypair.html#a311ab62b9b7187be519879d8c4cc1924',1,'godot::Keypair::new_random()'],['../classgodot_1_1_pubkey.html#aa9ccf03afa613628bcca250399c00ded',1,'godot::Pubkey::new_random()']]]
 ];
