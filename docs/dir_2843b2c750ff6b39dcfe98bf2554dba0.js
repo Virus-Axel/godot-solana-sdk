defa73b3dbcf65c9797726fe06610f4f0ba1e108
Virus-Axel <axel.benjaminsson@live.se>
2025-01-19 13:07:05 +0000

    Deploying to doxygen-docs from @ Virus-Axel/godot-solana-sdk@733144b710dfe24fa8242ff8d882a2134c35efdb 🚀

--- docs/dir_2843b2c750ff6b39dcfe98bf2554dba0.js
+++ docs/dir_2843b2c750ff6b39dcfe98bf2554dba0.js
@@ -2,7 +2,6 @@ var dir_2843b2c750ff6b39dcfe98bf2554dba0 =
 [
     [ "associated_token_account.hpp", "associated__token__account_8hpp_source.html", null ],
     [ "compute_budget.hpp", "compute__budget_8hpp_source.html", null ],
-    [ "mpl_candy_machine.hpp", "mpl__candy__machine_8hpp_source.html", null ],
     [ "mpl_token_metadata.hpp", "mpl__token__metadata_8hpp_source.html", null ],
     [ "spl_token.hpp", "spl__token_8hpp_source.html", null ],
     [ "spl_token_2022.hpp", "spl__token__2022_8hpp_source.html", null ],
---
+++ docs/dir_5f627655c57e0213242e4f793928fa51.js
@@ -0,0 +1,8 @@
+var dir_5f627655c57e0213242e4f793928fa51 =
+[
+    [ "candy_guard.hpp", "candy__guard_8hpp_source.html", null ],
+    [ "candy_guard_access_list.hpp", "candy__guard__access__list_8hpp_source.html", null ],
+    [ "candy_machine.hpp", "candy__machine_8hpp_source.html", null ],
+    [ "candy_machine_data.hpp", "candy__machine__data_8hpp_source.html", null ],
+    [ "config_line.hpp", "config__line_8hpp_source.html", null ]
+];
